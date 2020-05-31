7546d3640262de76c25c29c3a2101b1bef4fbabf
Jack Goode <jack.w.goode@icloud.com>
2020-05-31 02:05:37 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
+def countdown(integer_num)
+  seconds = integer_num
+    while seconds > 0
+      puts "#{seconds} SECOND(S)!"
+      seconds -= 1
+    end
+     
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(count_integer)
+    seconds = count_integer
+    while seconds > 0
+      puts "#{seconds} SECOND(S)!"
+      sleep 1
+      seconds -= 1
+    end
+     
+    "HAPPY NEW YEAR!"
 end
